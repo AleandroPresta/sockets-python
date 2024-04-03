774750d257ccefe0c433c12d2e4ecf9c1ee20e12
AleandroPresta <all96p@libero.it>
2024-04-03 19:53:14 +0200

    Corrected the formulation of the request. The test is still formulated badly so it won't pass.

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -1,3 +1,8 @@
+import sys
+import os
+# Add the parent directory of src to the Python path
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import pytest
 from src.browser import perform_request
 
@@ -23,8 +28,11 @@ def test_perform_request():
     # Restore stdout
     sys.stdout = sys.__stdout__
 
+    # Defining the expected result
+    expectedResult = 'HTTP/1.1 200 OK\nDate: Wed, 03 Apr 2024 17:34:34 GMT\nServer: Apache/2.4.52 (Ubuntu)\nLast-Modified: Mon, 15 May 2017 11:11:47 GMT\nETag: "80-54f8e1f004857"\nAccept-Ranges: bytes\nContent-Length: 128\nCache-Control: max-age=0, no-cache, no-store, must-revalidate\nPragma: no-cache\nExpires: Wed, 11 Jan 1984 05:00:00 GMT\nConnection: close\nContent-Type: text/html\n<h1>The First Page</h1>\n<p>\nIf you like, you can switch to the \n<a href="http://data.pr4e.org/page2.htm">\nSecond Page</a>.\n</p>'
+
     # Check if the expected output is received
-    assert "Example Domain" in printed_output  # Assuming "Example Domain" is part of the expected output
+    assert expectedResult in printed_output  # Assuming "Example Domain" is part of the expected output
 
 if __name__ == "__main__":
     pytest.main([__file__])
